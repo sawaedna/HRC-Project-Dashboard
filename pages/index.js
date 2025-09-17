107f2ec833fa8c71f9b4a716259208dd40295be7
sawaedna <sawaedna.ksa@gmail.com>
2025-09-17 22:28:49 +0300

    06

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,6 @@ export default function Home() {
       </Head>
       <Script src="https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js" strategy="beforeInteractive" />
       <Script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0" strategy="beforeInteractive" />
-
-      <Script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" strategy="beforeInteractive" />
-
-      {/* إضافة المكتبة الجديدة لمخطط Sunburst */}
-      <Script src="https://unpkg.com/sunburst-chart" strategy="beforeInteractive" />
-      
-
       <Script src="/js/app.js" strategy="afterInteractive" />
 
       <div className="container">
