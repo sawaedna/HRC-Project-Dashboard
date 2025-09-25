9995a7d92684230c5e35b4bbbc77d4d928d81d16
sawaedna <sawaedna.ksa@gmail.com>
2025-09-25 15:17:20 +0300

    007

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ad98343b7b261d45"; }
+/******/ 		__webpack_require__.h = function() { return "cfeff0b16bc73fcd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
